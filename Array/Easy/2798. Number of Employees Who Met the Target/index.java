82519598a3b8dfaf14da037873c225d436d08f6b
hiusnguyen201 <hiusnguyen201@gmail.com>
2024-07-18 15:10:00 +0700

    (Array): :zap: 1431 - Kids With the Greatest Number of Candies - Easy

--- Array/Easy/2798. Number of Employees Who Met the Target/index.java
+++ Array/Easy/2798. Number of Employees Who Met the Target/index.java
@@ -1,72 +1,46 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 class Solution {
-    public static int numberOfEmployeesWhoMetTarget1(int[] hours, int target) {
-        int length = hours.length;
-        Arrays.sort(hours);
+    public static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int n = candies.length;
+        List<Boolean> result = new ArrayList<Boolean>();
 
-        if (hours[length - 1] < target) {
-            return 0;
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            max = Math.max(max, candies[i]);
         }
 
-        for (int i = 0; i < length; i++) {
-            if (hours[i] >= target) {
-                return length - i;
-            }
+        for (int i = 0; i < n; i++) {
+            result.add(candies[i] + extraCandies >= max);
         }
 
-        return 0;
-    }
-
-    public static int numberOfEmployeesWhoMetTarget2(int[] hours, int target) {
-        int length = hours.length;
-        int count = 0;
-
-        for (int i = 0; i < length; i++) {
-            if (hours[i] >= target) {
-                count++;
-            }
-        }
-
-        return count;
+        return result;
     }
 
     public static void main(String args[]) {
         /**
-         * First Solution:
-         * - Run Time: 2 ms
+         * - Run Time: 1 ms
          * - Memory: 42.4 MB
          */
         // Example 1
-        int[] hours1 = { 0, 1, 2, 3, 4 };
-        int target1 = 2;
-        int result1 = numberOfEmployeesWhoMetTarget1(hours1, target1);
+        int[] candies1 = { 2, 3, 5, 1, 3 };
+        int extraCandies1 = 3;
+        List<Boolean> result1 = kidsWithCandies(candies1, extraCandies1);
         System.out.println(result1);
 
         // Example 2
-        int[] hours2 = { 5, 1, 4, 2, 2 };
-        int target2 = 6;
-        int result2 = numberOfEmployeesWhoMetTarget1(hours2, target2);
+        int[] candies2 = { 4, 2, 1, 1, 2 };
+        int extraCandies2 = 1;
+        List<Boolean> result2 = kidsWithCandies(candies2, extraCandies2);
         System.out.println(result2);
 
-        /**
-         * Second Solution:
-         * - Run Time: 0 ms
-         * - Memory: 42.8 MB
-         */
-
-        // Example 1
-        int[] hours3 = { 0, 1, 2, 3, 4 };
-        int target3 = 2;
-        int result3 = numberOfEmployeesWhoMetTarget2(hours3, target3);
+        // Example 3
+        int[] candies3 = { 12, 1, 12 };
+        int extraCandies3 = 10;
+        List<Boolean> result3 = kidsWithCandies(candies3, extraCandies3);
         System.out.println(result3);
-
-        // Example 2
-        int[] hours4 = { 5, 1, 4, 2, 2 };
-        int target4 = 6;
-        int result4 = numberOfEmployeesWhoMetTarget2(hours4, target4);
-        System.out.println(result4);
-
     }
 
 }
